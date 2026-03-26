695c0bc0e448e22a199bf242ee1c412cd6d45479
Tanzeela Waheed <itzzmetanzeela@gmail.com>
2026-03-26 01:58:53 -0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Home from './(pages)/home/Home'
-
+// Static meta title and description
+export const metadata = {
+  title: 'Pitaa',
+  description: 'This is a static meta description for my website homepage. Explore our services and features.',
+}
 const page = () => {
   return (
     <div>
